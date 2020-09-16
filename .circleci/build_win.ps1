02dbade2422d9493fa2e234dedb537894d9c4464
Daniel Kirchner <daniel@ekpyron.org>
2020-09-16 11:23:19 +0200

    Try different build command for performance evaluation.

--- .circleci/build_win.ps1
+++ .circleci/build_win.ps1
@@ -3,5 +3,6 @@ mkdir build
 cd build
 $boost_dir=(Resolve-Path $PSScriptRoot\..\deps\boost\lib\cmake\Boost-*)
 ..\deps\cmake\bin\cmake -G "Visual Studio 16 2019" -DBoost_DIR="$boost_dir\" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\upload" ..
-..\deps\cmake\bin\cmake --build . -j 6 --config Release
+msbuild solidity.sln /p:Configuration=Release /m:5 /v:minimal
+#..\deps\cmake\bin\cmake --build . -j 6 --config Release
 ..\deps\cmake\bin\cmake --build . -j 6 --target install --config Release
